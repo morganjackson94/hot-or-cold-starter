91ed39dfcfa458a699b99a58a1c32d0c9ad8d6fc
Morgan Jackson <contactmojack@gmail.com>
2015-09-14 13:58:23 -0500

    number generated on load

--- js/app.js
+++ js/app.js
@@ -5,26 +5,69 @@ $(document).ready(function(){
 
   var globalNum;
   var gameFeedback;
-  var userGuesscount = $("#count");
-  var guessList = $("#guessList");
+  var userGuesscount = $('#count');
+  var guessList = $('#guessList');
   var guessCounter = 0;
-  var userGuess = $("#userGuess").val();
+  
+  
+  
 
   
   /*--- User Creates New Game ---*/
   var newGame = function() {
-      genNum();
-      clearGuesses();
-      console.log("you have started a new game");
-    };
+    genNum();
+    clearGuesses();
+    console.log("you have started a new game");
+  };
 
-/*--- Generate Random Number ---*/
+  /*--- Generate Random Number ---*/
   var genNum = function() {
     var randomNumber = Math.floor(Math.random() * 100) +1;
     globalNum = randomNumber;
     console.log("Mystery Number is " + randomNumber);
     return randomNumber;   
   };
+
+  /*--- Clear Guesses ---*/
+  var clearGuesses = function() {
+    guessCounter = 0;
+    $("#guessList").empty();
+    $("#userGuess").empty();
+  };
+  
+  /*--- User Enters Number ---*/
+  var addGuess = function() {
+    var newGuess = $('<li><span>' + userGuess + '</span></li>');
+    $('#guessList').append(newGuess);
+    $(userGuess).val(''); 
+  }
+
+  $('.guessForm').on('submit', function(e) {
+    e.preventDefault();
+    console.log(userGuess);
+    addGuess();
+  });
+
+  
+
+  
+/*--- New game on load ---*/   
+  newGame();
+
+/*--- New game on click ---*/   
+  $('.new').on('click', function() {
+    newGame();
+  });
+
+
+/*--- App Provides Feedback ---*/
+
+/*--- App Tracks how many guesses user has made ---*/
+
+/*--- App Provides list of guessed numbers ---*/
+
+
+
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -38,33 +81,11 @@ $(document).ready(function(){
 
   	
 
-  /*--- Clear Guesses ---*/
-  var clearGuesses = function() {
-    guessCounter = 0;
-    $("#guessList").empty();
-    $("#userGuess").empty();
-  };
+  
 
 
 
-    /*--- User Enters Number ---*/
-    var userInput = function() {
-      var newGuess = $('<li><span>' + userGuess + '</span><li>');
-      $('#guessList').append(newGuess);
-      $(userGuess).val('');
-    };
-
-    /*--- New game on click ---*/   
-    $('.new').on('click', function() {
-      newGame();
-    });
-
-
-    /*--- App Provides Feedback ---*/
-
-    /*--- App Tracks how many guesses user has made ---*/
-
-    /*--- App Provides list of guessed numbers ---*/
+    
 
 });
 
